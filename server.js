40ce3dec8dcad0372680c0b54b1d966e1646d08c
Kassi Stumb <kassi.mcintosh@gmail.com>
2020-09-04 14:34:15 -0400

    added process.env.PORT to PORT

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const dbNotes = JSON.parse(rawData);
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
